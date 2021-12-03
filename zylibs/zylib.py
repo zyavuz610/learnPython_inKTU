973f978f4f0bd9db4e2694ff8dcb9343fffcf936
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2021-12-03 18:17:47 +0300

    Update zylib.py

--- zylibs/zylib.py
+++ zylibs/zylib.py
@@ -6,10 +6,14 @@ def isPrime(n):
             return False
     return True
 
+#-----------------------------------------------
+
 def allPrimes(n):
     for i in range(2, n):
         if isPrime(i):
             yield i
+
+################################################
 
 def isPerfect(n):
     sum = 0
@@ -18,16 +22,24 @@ def isPerfect(n):
             sum += i
     return sum == n
 
+#-----------------------------------------------
+
 def allPerfects(n):
     for i in range(1, n):
         if isPerfect(i):
             yield i
 
+################################################
+
 def isPalindrome(n):
     s = str(n)
     return s == s[::-1]
+
+#-----------------------------------------------
 
 def allPalindromes(n):
     for i in range(1, n):
         if isPalindrome(i):
             yield i
+
+################################################
