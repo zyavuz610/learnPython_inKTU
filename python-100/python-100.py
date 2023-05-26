3689d40572d27d8a92e0f0780fcd41bdee103eee
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2023-05-26 18:40:50 +0300

    Create python-100.py

---
+++ python-100/python-100.py
@@ -0,0 +1,35 @@
+"""
+Seri: Örneklerle Python Programlama
+
+  Python 100 - Python ile programlama - giriş
+    Programlama nedir?: bir takım girdileri olan ve bu girdileri işleyerek çıktı üreten kodlar topluluğudur.
+      bilgi + sayar
+Read, girdi
+Eval, işlem
+Print, çıktı
+Loop, 
+Repl,
+3+5 -> 8
+
+    Python prog. dili
+uzantısı py olan dosyalara yazılır ve python programı ile çalıştırılır (yorumlanır).
+
+    Kurulum, 
+      replit.com, 
+      ortamı tanıyalım, interaktif python
+    ilk örnek, merhaba dünya
+"""
+# print("Merhaba Dünya")
+# print('Merhaba Python!.....')
+print("Merhaba","Zafer","Yavuz",sep=",")
+
+name = "Ali"
+print("Merhaba",name)
+
+a = 5
+b = 7
+
+# burada iki sayıyı topladık
+c = a+b   # c = 5+7=12
+print("Toplam:",c)
+print(a,"+",b,"=",c)
